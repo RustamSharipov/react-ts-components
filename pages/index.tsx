902fdb7cdc308091f6ea357b883ad0ee1635f6c6
Rustam <rustam.sharipov@gmail.com>
2020-08-28 20:13:20 +0300

    App

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p>
+          Get started by editing
+        </p>
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
